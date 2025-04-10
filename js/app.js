96c93fb47fa522c6d43a545b28355c98beca5098
Javier Lesaca Medina <javilesaca@gmail.com>
2025-04-11 00:12:26 +0200

    Añadida funcion para ocultar cursor personalizado en dispositivos tactiles y añadido favicon.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,33 @@
 'use strict'
 
-$(document).ready(function(){
+$(document).ready(function () {
+    // Detectar si es dispositivo táctil
+    const esTactil = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 
-    $('body').on({'mousemove' : function(e){
-        console.clear()
-        let clienteX = e.originalEvent.clientX
-        let clienteY = e.originalEvent.clientY
-        $('#cursor').css({
-            'left' : (clienteX - 40) + 'px',
-            'top' : (clienteY - 40) + 'px'
-        })
-    }})
+    if (esTactil) {
+        $('#cursor').hide(); // Ocultar el cursor personalizado
+        $('body').css('cursor', 'auto'); // Restaurar el cursor por defecto
+        return; // Salir de la función
+    }
+
+    // Solo se ejecuta en ordenadores (no táctiles)
+    $('body').on({
+        'mousemove': function (e) {
+            let clienteX = e.originalEvent.clientX;
+            let clienteY = e.originalEvent.clientY;
+            $('#cursor').css({
+                'left': (clienteX - 40) + 'px',
+                'top': (clienteY - 40) + 'px'
+            });
+        }
+    });
 
     $('.a').on({
-        'mouseover' : function(){
-            $('#cursor').addClass('mini')
+        'mouseover': function () {
+            $('#cursor').addClass('mini');
         },
-        'mouseout' : function(){
-            $('#cursor').removeClass('mini')
+        'mouseout': function () {
+            $('#cursor').removeClass('mini');
         },
-    })
-
-})
+    });
+});
